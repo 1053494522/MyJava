e86dfccbdd775dc65e100cd052759842eeaa0c5c
Administrator <1053494522@qq.com>
2021-01-15 19:06:29 +0800

    test

--- src/com/MyJava/Demo01.java
+++ src/com/MyJava/Demo01.java
@@ -1,9 +1,17 @@
 package com.MyJava;
 
 public class Demo01 {
+    //有规律的数字计算
+  public static String test01(int number){
 
-    int i = 1;
-    int a = 2;
-    int b = 3;
-    int e = i+a;
+       int a = number;
+       int b = number*10+number;
+       int c = number*100+number*10+number;
+       int d = number*1000+number*100+number*10+number;
+       int e = number*10000+number*1000+number*100+number*10+number;
+       int sun =a+b+c+d+e+d+c+b+a;
+       String end = a+"+"+b+"+"+c+"+"+d+"+"+e+"+"+d+"+"+c+"+"+b+"+"+a+"="+sun;
+       return end;
+    }
+
 }
--- src/com/MyJava/Main.java
+++ src/com/MyJava/Main.java
@@ -1,7 +1,11 @@
 package com.MyJava;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Demo01.class);
-    }
+        Scanner sn = new Scanner(System.in);
+        int number = sn.nextInt();
+        System.out.println(Demo01.test01(number));
+        }
 }
