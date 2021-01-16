be7afbc45c46bab816312191be4f80870014ba01
Administrator <1053494522@qq.com>
2021-01-16 09:24:44 +0800

    test

---
+++ src/com/MyJava/Demo03.java
@@ -0,0 +1,20 @@
+package com.MyJava;
+
+import java.util.Random;
+
+public class Demo03 {
+    public static String test03(String a){
+        if (a.equals("GET")){
+            String str = "0123456789";
+            StringBuilder sb = new StringBuilder(6);
+            for (int i = 0; i < 6; i++) {
+                char ch = str.charAt(new Random().nextInt(str.length()));
+                sb.append(ch);
+            }
+            return sb.toString();
+        }else {
+            return null;
+        }
+
+    }
+}
