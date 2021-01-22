51ea481033e3df784aa37094799db0afe6474371
Administrator <1053494522@qq.com>
2021-01-22 21:36:31 +0800

    test

---
+++ src/com/MyJava/Demo10.java
@@ -0,0 +1,17 @@
+package com.MyJava;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Demo10 {
+    public static void main(String[] args) throws ParseException {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date data = simpleDateFormat.parse("2020-12-10 10:30:00");
+        calendar.setTime(data);
+        String timeStr = (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")).format(calendar.getTime());
+        System.out.println(timeStr);
+    }
+}
