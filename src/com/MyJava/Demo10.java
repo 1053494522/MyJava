921c6f2989824f4e8e51f6d96edf34b3fa4c832f
Administrator <1053494522@qq.com>
2021-01-26 19:30:20 +0800

    test

--- src/com/MyJava/Demo10.java
+++ src/com/MyJava/Demo10.java
@@ -8,9 +8,14 @@ import java.util.Date;
 public class Demo10 {
     public static void main(String[] args) throws ParseException {
         Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date data = simpleDateFormat.parse("2020-12-10 10:30:00");
-        calendar.setTime(data);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        try{
+            Date data = simpleDateFormat.parse("2020年12月10日 10:30:00");
+            calendar.setTime(data);
+        }catch (ParseException e){
+            e.printStackTrace();
+        }
+
         String timeStr = (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")).format(calendar.getTime());
         System.out.println(timeStr);
     }
