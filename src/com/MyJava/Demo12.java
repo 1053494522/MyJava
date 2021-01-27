07d9ae46dfd1b0ada8655ad50635f846d0faa0f0
Administrator <1053494522@qq.com>
2021-01-27 20:17:17 +0800

    test

--- src/com/MyJava/Demo12.java
+++ src/com/MyJava/Demo12.java
@@ -1,4 +1,26 @@
 package com.MyJava;
 
+import java.util.Scanner;
+
+//判断一个字符串中的"( )"是否配对
 public class Demo12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int a = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char b = str.charAt(i);
+            if (b == '(') {
+                a++;
+            } else if (b == ')') {
+                a--;
+            }
+        }
+        if (a == 0) {
+            System.out.println("您输入的内容，括号完全配对");
+        } else {
+            System.out.println("您输入的内容，括号不配对");
+        }
+
+    }
 }
