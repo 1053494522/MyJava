f12e08bf2fd32f321f651022176133794beede2c
Administrator <1053494522@qq.com>
2021-01-31 11:43:39 +0800

    test

---
+++ src/com/MyJava/Demo18.java
@@ -0,0 +1,31 @@
+package com.MyJava;
+
+import java.util.Scanner;
+
+//回文数
+public class Demo18 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        test.hui(str);
+    }
+}
+
+class test {
+    public static void hui(String str) {
+        boolean flag = true;
+        for (int i = 0, j = str.length() - 1; i < j; i++, j--) {
+            if (str.charAt(i) == str.charAt(j)) {
+                continue;
+            } else {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            System.out.println("字符串" + str + ",是一个字符串");
+        } else {
+            System.out.println("字符串" + str + ",不是一个字符串");
+        }
+    }
+}
