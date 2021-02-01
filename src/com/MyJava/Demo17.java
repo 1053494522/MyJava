aeb6e7c24da0da2dd804240f8ecc625dda91f385
Administrator <1053494522@qq.com>
2021-02-01 20:27:31 +0800

    test

--- src/com/MyJava/Demo17.java
+++ src/com/MyJava/Demo17.java
@@ -1,23 +1,33 @@
 package com.MyJava;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.util.Scanner;
 
 //输出指定路径下的所有文件名
 public class Demo17 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        File f = new File(str);
-        method(f);
-
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        String rootPath = str;
+        System.out.println("目录【"+str+"】中有以下文件");
+        File file = new File(rootPath);
+        print(0, file);
 
     }
-    private static void method(File f) {
-        if(f.isDirectory()) {
-            File[] fs = f.listFiles();
-            for (File f1 : fs) {
-            System.out.println(f1);
+
+    public static void print(int n, File file) {
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("　　");
+        }
+
+        System.out.println(file.getName());
+
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            for (File subFile : files) {
+                print(n + 1, subFile);
             }
         }
     }
